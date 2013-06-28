72fce231b333b9ac8b74ebcc0fe0bf0382bd5d9e
Greg McW <greg.mcwhirter@gmail.com>
2013-06-28 20:40:35 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Etps::Application.config.secret_key_base = '68756516043b89e626adc98c99a99fbe85fb4ef47160a4bac80c9a28cc6be9e5deac54156f3eb6f31545f73d7832a3bf227208a3e1025b7d8dd205e25d890e1a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Etps::Application.config.session_store :cookie_store, key: '_etps_session'
