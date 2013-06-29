6c5138ede6007ab3d7d4b975484431503721ffdc
Greg McW <greg.mcwhirter@gmail.com>
2013-06-29 03:10:31 +0000

    Friday June 28, 2013 -- Added first tests

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe "User" do
+  describe "Login" do
+    it "should have the content 'login'" do
+    	visit '/user/login'
+    	page.should have_content('login')
+    end
+  end
+end
+
+describe "Bluebird" do
+	describe "Busroutes" do
+		it "should mention Bus Routes" do
+			visit '/bluebird/busroutes'
+			page.should have_selector('h2', :text => 'Bus Routes')
+		end
+	end
+end
